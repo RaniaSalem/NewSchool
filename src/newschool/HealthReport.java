9cbb5f578ea806ec6588b62ad7bf4a591a8bd044
eng doaa <eng doaa@doaa-PC>
2015-12-22 21:18:23 +0200

    Doaa commit doaa

--- src/newschool/HealthReport.java
+++ src/newschool/HealthReport.java
@@ -10,5 +10,43 @@ package newschool;
  * @author eng doaa
  */
 public class HealthReport {
+     private int reportId;
+    private String reportContent;
+    private int healthId;
+    private int student_ID;
+
+    public int getReportId() {
+        return reportId;
+    }
+
+    public void setReportId(int reportId) {
+        this.reportId = reportId;
+    }
+
+    public String getReportContent() {
+        return reportContent;
+    }
+
+    public void setReportContent(String reportContent) {
+        this.reportContent = reportContent;
+    }
+
+    public int getHealthId() {
+        return healthId;
+    }
+
+    public void setHealthId(int healthId) {
+        this.healthId = healthId;
+    }
+
+    public int getStudent_ID() {
+        return student_ID;
+    }
+
+    public void setStudent_ID(int student_ID) {
+        this.student_ID = student_ID;
+    }
+
+   
     
 }
