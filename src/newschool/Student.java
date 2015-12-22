daf6454b487946ee42a70a96236d7d7033ed7a8e
Noura <Noura@Nourhan>
2015-12-22 21:09:31 +0200

    this is student and student pojo

--- src/newschool/Student.java
+++ src/newschool/Student.java
@@ -1,5 +1,98 @@
 package newschool;
 
+import java.util.Date;
+
 public class Student {
+     private int studentId;
+   private int studentLevel;
+   private String firstName;
+   private String midName;
+   private String lastName;
+   private String address;
+   private String gender;
+   private Date  dateOfBirth;
+   private String  phone;
+   private Parent parentId;
+
+    public int getStudentId() {
+        return studentId;
+    }
     
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getStudentLevel() {
+        return studentLevel;
+    }
+
+    public void setStudentLevel(int studentLevel) {
+        this.studentLevel = studentLevel;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMidName() {
+        return midName;
+    }
+
+    public void setMidName(String midName) {
+        this.midName = midName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+   
+
+    public int getParentId() {
+        return  parentId.parentId;
+    }
+
+    public void setParentId(int parentId) {
+        this.parentId.ParentId = parentId;
+    }
 }
