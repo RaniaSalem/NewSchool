aae397747e3f9659cd01c9fac09335f749157a0d
eng doaa <eng doaa@doaa-PC>
2015-12-22 03:23:27 +0200

    Doaa commit 3

---
+++ src/newschool/HealthVisitor.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newschool;
+
+import java.util.Date;
+
+/**
+ *
+ * @author eng doaa
+ */
+public class HealthVisitor {
+     private int health_ID;
+    private Date dateOfHire;
+    private String qualification;
+
+    private String firstName;
+    private String midName;
+    private String lastName;
+    private String address;
+    private String gender;
+    private Date dateOfBirth;
+    private String phone;
+
+    public int getHealth_ID() {
+        return health_ID;
+    }
+
+    public void setHealth_ID(int health_ID) {
+        this.health_ID = health_ID;
+    }
+
+    public Date getDateOfHire() {
+        return dateOfHire;
+    }
+
+    public void setDateOfHire(Date dateOfHire) {
+        this.dateOfHire = dateOfHire;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMidName() {
+        return midName;
+    }
+
+    public void setMidName(String midName) {
+        this.midName = midName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+   
+}
