494cfb831be60e08744d9f1a26dd1bd872479fdd
Smart <Smart@WINDOWS-4BF6EE2>
2015-12-26 01:34:52 +0200

    Mahmoud

--- src/com/school/demo/ManagerDemo.java
+++ src/com/school/demo/ManagerDemo.java
@@ -19,7 +19,7 @@ public class ManagerDemo {
         managerDaoImpl.editDataMan(5, "Mahmouddddddd", "2015/12/1", "high", "Manager", "Mohamed", "Mostafa", "Sharqia", "Male", "1666/12/1", "123456789", "Manager");//update data(first_name) according to (man_id)in table Manager
 //        operation on Teacher
         managerDaoImpl.insertDataTeacher(53, "2015/12/1", "high", "Teacher", "sepci", "Mahmoud", "Mohamed", "Mostafa", "Sharqia", "Male", "1666/12/1", "123456789", "Teacher");
-        managerDaoImpl.removeDataTeacher(53);
+        managerDaoImpl.removeDataTeacher(53); 
         managerDaoImpl.editDataTeacher(53, "2015/12/1", "high", "Teacher", "sepcihhhhh", "Mahmoud", "Mohamed", "Mostafa", "Sharqia", "Male", "1666/12/1", "123456789", "Teacher");
         printTeacherDetails(53);//print data of Teacher
 //Students
