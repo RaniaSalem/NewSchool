f881e3572605d81be0ca36a6dec622ddb0d839db
Smart <Smart@WINDOWS-4BF6EE2>
2015-12-25 19:47:50 +0200

    Mahmoud

--- src/com/school/dao/ManagerDaoImpl.java
+++ src/com/school/dao/ManagerDaoImpl.java
@@ -4,46 +4,42 @@ import com.school.pojo.Manager;
 import java.sql.Connection;
 import java.sql.ResultSet;
 
-public class ManagerDaoImpl implements ManagerDao {
+public class ManagerDaoImpl extends DataBaseDaoImp implements ManagerDao {
 
     @Override
     public void InsertDataMan(Object manId, Object dateOfHire, Object qualification, Object postion, Object firstName, Object midName, Object lastName, Object address, Object gender, Object dateBirth, Object phone, Object type) {
-        DataBaseDaoImp dataBaseDaoImpl = new DataBaseDaoImp();
-        Connection conn = dataBaseDaoImpl.openConnection();
+        Connection conn = openConnection();
         String sql = "insert into Manager (MAN_ID,`DATE_OF_HIRE`,`QUALIFICATION`,`POSITION`,`FIRST_NAME`,`MID_NAME`,`LAST_NAME`,`ADDRESS`,`GENDER`,`DATE_OF_BIRTH`,`PHONE`,`TYPE`)"
                 + "values (" + manId + ",'" + dateOfHire + "','" + qualification + "','" + postion + "','" + firstName + "','" + midName + "','" + lastName + "','" + address + "','" + gender + "','" + dateBirth + "','" + phone + "','" + type + "')";
-        dataBaseDaoImpl.updateQuery(sql, conn);
-        dataBaseDaoImpl.closeConnection(conn);
+        updateQuery(sql, conn);
+        closeConnection(conn);
     }
 
     @Override
     public void removeDataMan(Object manId) {
-        DataBaseDaoImp dataBaseDaoImpl = new DataBaseDaoImp();
-        Connection conn = dataBaseDaoImpl.openConnection();
+        Connection conn = openConnection();
         String sql = "delete from Manager where MAN_ID = " + manId;
-        dataBaseDaoImpl.updateQuery(sql, conn);
-        dataBaseDaoImpl.closeConnection(conn);
+        updateQuery(sql, conn);
+        closeConnection(conn);
     }
 
     @Override
     public void editDataMan(Object manId, Object firstName, Object dateOfHire, Object qualification, Object postion, Object midName, Object lastName, Object address, Object gender, Object dateBirth, Object phone, Object type) {
-        DataBaseDaoImp dataBaseDaoImpl = new DataBaseDaoImp();
-        Connection conn = dataBaseDaoImpl.openConnection();
+        Connection conn = openConnection();
         String sql = "update Manager set FIRST_NAME = '" + firstName + "' , `DATE_OF_HIRE`='" + dateOfHire + "' , `QUALIFICATION` = '" + qualification + "' , `POSITION`='" + postion + "' , `MID_NAME`='" + midName + "' , `LAST_NAME`='" + lastName + "' , `ADDRESS`='" + address + "' , `GENDER`='" + gender + "' , `DATE_OF_BIRTH`='" + dateBirth + "' , `PHONE`='" + phone + "' , `TYPE`='" + type + "'  where MAN_ID = " + manId;
         System.out.println("newschool.ManagerDaoImpl.editDataMan():" + sql);
-        dataBaseDaoImpl.updateQuery(sql, conn);
-        dataBaseDaoImpl.closeConnection(conn);
+        updateQuery(sql, conn);
+        closeConnection(conn);
     }
 
     @Override
     public Manager viewManagerScreen(int manId) {
-        DataBaseDaoImp dataBaseDaoImpl = new DataBaseDaoImp();
-        Connection conn = dataBaseDaoImpl.openConnection();
+        Connection conn = openConnection();
 //        String sql1 = "select FIRST_NAME from Manager where MAN_ID = " + manId;
         String sql = "select `DATE_OF_HIRE`,`QUALIFICATION`,`POSITION`,`FIRST_NAME`,`MID_NAME`,`LAST_NAME`,`ADDRESS` "
                 + ",`GENDER`,`DATE_OF_BIRTH`,`PHONE`,`TYPE` ,`MAN_ID` "
                 + " from manager where MAN_ID = " + manId;
-        ResultSet rs = dataBaseDaoImpl.excuteQuery(sql, conn);
+        ResultSet rs = excuteQuery(sql, conn);
         Manager m = new Manager();
         try {
             while (rs.next()) {
@@ -63,7 +59,7 @@ public class ManagerDaoImpl implements ManagerDao {
                 m.setType(rs.getString(11));//TYPE
                 m.setManId(rs.getInt(12));
             }
-            dataBaseDaoImpl.closeConnection(conn);
+            closeConnection(conn);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
