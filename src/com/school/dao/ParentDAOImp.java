fc5712fc8fe4557d233d46c246934997275af629
manal pc <manal pc@Manal>
2015-12-26 01:18:00 +0200

    Manalcommit

--- src/com/school/dao/ParentDAOImp.java
+++ src/com/school/dao/ParentDAOImp.java
@@ -2,6 +2,7 @@ package com.school.dao;
 
 import com.school.pojo.Parent;
 import java.sql.Connection;
+import java.util.Date;
 
 /**
  *
@@ -11,6 +12,7 @@ import java.sql.Connection;
 
 public class ParentDAOImp  extends DataBaseDaoImp implements ParentDAO{
   Connection conn = openConnection();
+  /*
     @Override
     public void viewMarkaStudent(Parent parent) {
     }
@@ -26,14 +28,14 @@ public class ParentDAOImp  extends DataBaseDaoImp implements ParentDAO{
     @Override
     public void viewScheduleExam(Parent parent) {
     }
-
-    @Override
-    public void addDataParent(Parent parent) {
-    }
-
-    @Override
+  @Override
     public void viewScheduleSubject(Parent parent) {
     }
+*/
+  
+  
+
+    
 
     @Override
     public void deleteDataParent(int parentId) {
@@ -45,5 +47,18 @@ public class ParentDAOImp  extends DataBaseDaoImp implements ParentDAO{
     @Override
     public void updateDataParent(Parent parent) {
     }
+
+    @Override
+    public void insertDataParent(int parentId, String firstName, String lastName, String midName, String address, String gender, Object dateOfBirth, String Phone, int studentId) {
+        String sql = "insert into Parents (PARENT_ID,`FIRST_NAME`,`MID_NAME`,`LAST_NAME`,`ADDRESS`,`GENDER`,`DATE_OF_BIRTH`,`PHONE`,`STUD_ID`) "
+                + " values (" + parentId + ",'" + firstName + "','" + midName + "','" + lastName+ "','" + address+ "','" + gender + "','" + dateOfBirth + "','" + Phone+ "','" + studentId + "')";
+        updateQuery(sql, conn);
+        closeConnection(conn);
+        
+    }
+
+   
+
+    
     
 }
