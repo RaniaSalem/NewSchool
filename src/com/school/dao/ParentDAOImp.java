e6f09858bf0069a9437394ec324bf9fd84ff93b5
manal pc <manal pc@Manal>
2015-12-25 18:35:50 +0200

    Manalcommit

--- src/com/school/dao/ParentDAOImp.java
+++ src/com/school/dao/ParentDAOImp.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.school.dao;
 
 import com.school.pojo.Parent;
@@ -15,42 +10,34 @@ public class ParentDAOImp implements ParentDAO{
 
     @Override
     public void viewMarkaStudent(Parent parent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void viewAttendanceStudent(Parent parent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void viewReportStudent(Parent parent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void viewScheduleExam(Parent parent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void addDataParent(Parent parent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void viewScheduleSubject(Parent parent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void deleteDataParent(Parent parent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void updateDataParent(Parent parent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
