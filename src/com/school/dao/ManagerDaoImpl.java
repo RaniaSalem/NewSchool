a42613b3f46e00b55f5aa71a1231dc638f65a348
Smart <Smart@WINDOWS-4BF6EE2>
2015-12-25 22:00:14 +0200

    Mahmoud

--- src/com/school/dao/ManagerDaoImpl.java
+++ src/com/school/dao/ManagerDaoImpl.java
@@ -3,14 +3,16 @@ package com.school.dao;
 import com.school.pojo.Manager;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ManagerDaoImpl extends DataBaseDaoImp implements ManagerDao {
 
     @Override
-    public void InsertDataMan(Object manId, Object dateOfHire, Object qualification, Object postion, Object firstName, Object midName, Object lastName, Object address, Object gender, Object dateBirth, Object phone, Object type) {
+    public void insertDataMan(Object manId, Object dateOfHire, Object qualification, Object postion, Object firstName, Object midName, Object lastName, Object address, Object gender, Object dateBirth, Object phone, Object type) {
         Connection conn = openConnection();
-        String sql = "insert into Manager (MAN_ID,`DATE_OF_HIRE`,`QUALIFICATION`,`POSITION`,`FIRST_NAME`,`MID_NAME`,`LAST_NAME`,`ADDRESS`,`GENDER`,`DATE_OF_BIRTH`,`PHONE`,`TYPE`)"
-                + "values (" + manId + ",'" + dateOfHire + "','" + qualification + "','" + postion + "','" + firstName + "','" + midName + "','" + lastName + "','" + address + "','" + gender + "','" + dateBirth + "','" + phone + "','" + type + "')";
+        String sql = "insert into Manager (MAN_ID,`DATE_OF_HIRE`,`QUALIFICATION`,`POSITION`,`FIRST_NAME`,`MID_NAME`,`LAST_NAME`,`ADDRESS`,`GENDER`,`DATE_OF_BIRTH`,`PHONE`,`TYPE`) "
+                + " values (" + manId + ",'" + dateOfHire + "','" + qualification + "','" + postion + "','" + firstName + "','" + midName + "','" + lastName + "','" + address + "','" + gender + "','" + dateBirth + "','" + phone + "','" + type + "')";
         updateQuery(sql, conn);
         closeConnection(conn);
     }
@@ -65,5 +67,43 @@ public class ManagerDaoImpl extends DataBaseDaoImp implements ManagerDao {
         }
         return m;
 //    }
+    }
+
+    @Override
+    public Manager getAllManager() {
+        Connection conn = openConnection();
+//        String sql1 = "select FIRST_NAME from Manager where MAN_ID = " + manId;
+        String sql = "select `DATE_OF_HIRE`,`QUALIFICATION`,`POSITION`,`FIRST_NAME`,`MID_NAME`,`LAST_NAME`,`ADDRESS` "
+                + ",`GENDER`,`DATE_OF_BIRTH`,`PHONE`,`TYPE` ,`MAN_ID` "
+                + " from manager ";
+        ResultSet rs = excuteQuery(sql, conn);
+//        List list= (List)rs;
+//        for (int i = 0; i < list.size(); i++) {
+//            list.
+//        }
+        Manager m = new Manager();
+        try {
+            while (rs.next()) {
+//                String firstName = rs.getString(1);
+//                System.out.println("firstName:::" + firstName);
+//                m.setFirstName(firstName);
+                m.setDateOfHire(rs.getDate(1));//DATE_OF_HIRE
+                m.setQualification(rs.getString(2));//QUALIFICATION
+                m.setPosition(rs.getString(3));//POSITION
+                m.setFirstName(rs.getString(4));//FIRST_NAME
+                m.setMidName(rs.getString(5));//MID_NAME
+                m.setLastName(rs.getString(6));//LAST_NAME
+                m.setAddress(rs.getString(7));//ADDRESS
+                m.setGender(rs.getString(8));//GENDER
+                m.setDateOfBirth(rs.getDate(9));//DATE_OF_BIRTH
+                m.setPhone(rs.getString(10));//PHONE
+                m.setType(rs.getString(11));//TYPE
+                m.setManId(rs.getInt(12));
+            }
+            closeConnection(conn);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return m;
     }
 }
--- src/com/school/demo/ManagerDemo.java
+++ src/com/school/demo/ManagerDemo.java
@@ -6,9 +6,10 @@ import com.school.pojo.Manager;
 public class ManagerDemo {
 
     public static void main(String[] args) {
+        printAllDataManagerDetails();//getAll Data of Manager
         printManagerDetails(1);//select data in table Manager
         ManagerDaoImpl managerDaoImpl = new ManagerDaoImpl();
-        managerDaoImpl.InsertDataMan(16, "2015/12/1", "high", "Manager", "Mahmoud", "Mohamed", "Mostafa", "Sharqia", "Male", "1666/12/1", "123456789", "High Manager");//insert data(man_id) in table Manager
+        managerDaoImpl.insertDataMan(16, "2015/12/1", "high", "Manager", "Mahmoud", "Mohamed", "Mostafa", "Sharqia", "Male", "1666/12/1", "123456789", "High Manager");//insert data(man_id) in table Manager
         managerDaoImpl.removeDataMan(13);//delete data(man_id) in table Manager
         managerDaoImpl.editDataMan(5, "Mahmouddddddd", "2015/12/1", "high", "Manager", "Mohamed", "Mostafa", "Sharqia", "Male", "1666/12/1", "123456789", "Manager");//update data(first_name) according to (man_id)in table Manager
     }
@@ -31,4 +32,23 @@ public class ManagerDemo {
         System.out.println("mm.getDateOfHire() :" + manager.getDateOfHire());//select data(FirstName) in table Manager
         System.out.println("mm.getManId() :" + manager.getManId());//select data(FirstName) in table Manager   
     }
+    
+    private static void printAllDataManagerDetails() {
+        ManagerDaoImpl managerDaoImpl = new ManagerDaoImpl();
+        Manager manager = managerDaoImpl.getAllManager();
+        System.out.println("m.getFirstName() :" + manager.getFirstName());//select data(FirstName) in table Manager
+        System.out.println("m.getAddress() :" + manager.getAddress());//select data(FirstName) in table Manager
+        System.out.println("m.getGender() :" + manager.getGender());//select data(FirstName) in table Manager
+        System.out.println("m.getLastName() :" + manager.getLastName());//select data(FirstName) in table Manager
+        System.out.println("m.getMidName() :" + manager.getMidName());//select data(FirstName) in table Manager
+        System.out.println("m.getLastName() :" + manager.getLastName());//select data(FirstName) in table Manager
+        System.out.println("m.getPhone() :" + manager.getPhone());//select data(FirstName) in table Manager
+        System.out.println("m.getPosition() :" + manager.getPosition());//select data(FirstName) in table Manager
+        System.out.println("m.getQualification() :" + manager.getQualification());//select data(FirstName) in table Manager
+        System.out.println("m.getType() :" + manager.getType());//select data(FirstName) in table Manager
+        System.out.println("m.getDateOfBirth() :" + manager.getDateOfBirth());//select data(FirstName) in table Manager
+        System.out.println("mm.getDateOfHire() :" + manager.getDateOfHire());//select data(FirstName) in table Manager
+        System.out.println("mm.getManId() :" + manager.getManId());//select data(FirstName) in table Manager   
+    }
+    
 }
