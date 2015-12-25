90a2b115e1882a69ef99e43a8206503d52ad5747
manal pc <manal pc@Manal>
2015-12-26 01:16:42 +0200

    Manalcommit

--- src/com/school/dao/ParentDAO.java
+++ src/com/school/dao/ParentDAO.java
@@ -7,12 +7,15 @@ import com.school.pojo.Parent;
  * @author manal pc
  */
 public interface ParentDAO {
+    /*
     public void viewMarkaStudent(Parent parent);
     public void viewAttendanceStudent(Parent parent);
     public void viewReportStudent(Parent parent);
     public void viewScheduleExam(Parent parent);
-    public void addDataParent(Parent parent);
     public void viewScheduleSubject(Parent parent);
+    */
+    public void insertDataParent(int parentId , String firstName,String lastName,String midName,String address,String gender, Object dateOfBirth, String Phone ,int studentId);
+    
     public void deleteDataParent(int parentId);
     public void updateDataParent(Parent parent);
     
